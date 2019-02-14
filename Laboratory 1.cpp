80911a54a108338bb1a1e711627d831fbc7bbdea
Bryan Suryaraso Gani <bryansuryarasogani@gmail.com>
2019-02-14 12:17:45 +0800

    Added New Message and Escape Sequence for Quotation Marks

--- Laboratory 1.cpp
+++ Laboratory 1.cpp
@@ -15,19 +15,25 @@ int main(int argc, char const *argv[]) {
   do {
     //Randomizer
     srand(time(0));
-    int message = rand() % 4 + 1;
+    int message = rand() % 6 + 1;
 
     //Print Cookie Opening Sentence
     cout << "Open a Fortune Cookie" << '\n';
     cout << message << '\n';
 
     //Message
-    if (message == 0) {
-      cout << "You cannot love life until you live the life you love." << '\n';
-    } else if (message == 1) {
-      cout << "Put your unhappiness aside. Life is beautiful, be happy." << '\n';
+    if (message == 1) {
+      cout << "\"You cannot love life until you live the life you love.\"" << '\n';
+    } else if (message == 2) {
+      cout << "\"Put your unhappiness aside. Life is beautiful, be happy.\"" << '\n';
+    } else if (message == 3){
+      cout << "\"If you speak honestly, everyone will listen.\"" << '\n';
+    } else if (message == 4){
+      cout << "\"Make a wise choice everyday.\"" << '\n';
+    } else if (message == 5){
+      cout << "\"Good things take time.\"" << '\n';
     } else {
-      cout << "Make a wise choice everyday." << '\n';
+      cout << "\"You create your own stage ... the audience is waiting.\"" << '\n';
     }
 
     //Asking User
