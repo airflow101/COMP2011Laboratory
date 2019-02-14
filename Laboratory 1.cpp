9a75f3a55e9dda8cecbbfed0c16212f672837fb0
Bryan Suryaraso Gani <bryansuryarasogani@gmail.com>
2019-02-14 10:06:44 +0800

    Looping and Questioning

--- Laboratory 1.cpp
+++ Laboratory 1.cpp
@@ -5,8 +5,25 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+  //Variable
+  char answer;
+
+  //Randomizer
   srand(time(0));
   int message = rand() % 4;
+
+  //Print First Sentence
   cout << "Fortune Cookie" << '\n';
+
+  //Looping
+  do {
+    //Print Cookie Opening Sentence
+    cout << "Open a Fortune Cookie" << '\n';
+
+    //Asking User
+    cout << "Crack open another one?" << '\n';
+    cin >> answer;
+  } while(answer == 'y');
+
   return 0;
 }
