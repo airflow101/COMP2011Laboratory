ba7d1371d65f9a2d539102df7f32a4f22183fc90
Bryan Suryaraso Gani <bryansuryarasogani@gmail.com>
2019-02-14 10:14:25 +0800

    If ElseIf Else and Program Closing

--- Laboratory 1.cpp
+++ Laboratory 1.cpp
@@ -8,22 +8,35 @@ int main(int argc, char const *argv[]) {
   //Variable
   char answer;
 
-  //Randomizer
-  srand(time(0));
-  int message = rand() % 4;
-
   //Print First Sentence
   cout << "Fortune Cookie" << '\n';
 
   //Looping
   do {
+    //Randomizer
+    srand(time(0));
+    int message = rand() % 4 + 1;
+
     //Print Cookie Opening Sentence
     cout << "Open a Fortune Cookie" << '\n';
+    cout << message << '\n';
+
+    //Message
+    if (message == 0) {
+      cout << "You cannot love life until you live the life you love." << '\n';
+    } else if (message == 1) {
+      cout << "Put your unhappiness aside. Life is beautiful, be happy." << '\n';
+    } else {
+      cout << "Make a wise choice everyday." << '\n';
+    }
 
     //Asking User
     cout << "Crack open another one?" << '\n';
     cin >> answer;
+    cout << endl;
   } while(answer == 'y');
 
+  //Closing Program
+  cout << "Bye!" << '\n';
   return 0;
 }
