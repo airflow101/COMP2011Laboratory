2a56d28568474b4884268e47301412f636622def
Bryan Suryaraso Gani <bryansuryarasogani@gmail.com>
2019-02-21 00:10:27 +0800

    Final Simple Version

--- Laboratory2.cpp
+++ Laboratory2.cpp
@@ -13,22 +13,39 @@ int main(int argc, char const *argv[]) {
   //Variables declaration
   int leftcannibals, rightcannibals, //amount of cannibals per side
    leftmissionaries, rightmissionaries, //amount of missionaries per side
-   boatposition, //boat postion (0 = right, 1 = left)
-   boatcannibals, boatmissionaries /*amount of cannibals and missionaries onboard*/;
+   boatstatus, //boat postion (0 =  boarding from right, 1 = landing  at left, 2 = boarding from left, 3 = landing at right)
+   boatcannibals, boatmissionaries, //amount of cannibals and missionaries onboard
+   promptcannibals, promptmissionaries; //amount of cannibals and missionaries inputed
   char confirmationQA;//confirming player input
 
   //Initial Condition
   leftcannibals = 0; rightcannibals = 3;//Right-side
   leftmissionaries = 0; rightmissionaries = 3;//Left-side
-  boatposition = 0; boatcannibals = 0; boatmissionaries = 0;//Boat
+  boatcannibals = 0; boatmissionaries = 0;//Boat
 
   //Game Loop
-  do {
+  for (boatstatus = 0; ; boatstatus++) {
+    if (boatstatus % 4 == 1){
+      if ((rightcannibals > rightmissionaries && rightmissionaries > 0) || ((boatcannibals + leftcannibals) > (boatmissionaries + leftmissionaries) && (boatmissionaries + leftmissionaries > 0))) {
+        cout << "Cannibals eat a missionaries. You FAILED!" << '\n';
+        break;
+      }
+    }
+    else if (boatstatus % 4 == 3){
+      if ((leftcannibals > leftmissionaries && leftmissionaries > 0) || ((boatcannibals + rightcannibals) > (boatmissionaries + rightmissionaries) && (boatmissionaries + rightmissionaries > 0))) {
+        cout << "Cannibals eat a missionaries. You FAILED!" << '\n';
+        break;
+      }
+    }
+    else if (leftcannibals == 3 && leftmissionaries == 3) {
+      cout << "Congratulations! You manage to move everyone to the destination!" << '\n';
+      break;
+    }
     //Condition at Location
     cout << "Left side --------------------------- Right side" << '\n';
     cout << leftcannibals << "C" << leftmissionaries << "M"; //Left side condition
-    if (boatposition == 1) {
-      cout << ", Boat(" << boatcannibals << "C" << boatmissionaries << "M" << ")";
+    if (boatstatus % 4 == 1 || boatstatus % 4 == 2) {
+      cout << ", Boat(" << boatcannibals << "C" << boatmissionaries << "M)";
       for (int i = 0; i < 22; i++) {
         cout << " ";
       }
@@ -38,37 +55,72 @@ int main(int argc, char const *argv[]) {
       }
     }
     cout << rightcannibals << "C" << rightmissionaries << "M"; //Right side condition
-    if (boatposition == 0) {
-      cout << ", Boat(" << boatcannibals << "C" << boatmissionaries << "M" << ")" << '\n';
+    if (boatstatus % 4 == 0 || boatstatus % 4 == 3) {
+      cout << ", Boat(" << boatcannibals << "C" << boatmissionaries << "M)" << '\n';
+    }
+    else{
+      cout << '\n';
     }
 
-    //Prompt - Getting ON The board
+    //Prompt - Getting ON/OFF The Boat
+    string boatcondition;
+    if (boatstatus % 4 == 0 || boatstatus % 4 == 2) {
+      boatcondition = "ON";
+    } else {
+      boatcondition  = "OFF";
+    }
     do {
-      cout << "How many missionaries get ON the boat?" << '\n';
-      cin >> boatmissionaries;//Misionaries
-      cout << "How many cannibals get ON the boat?" << '\n';
-      cin >> boatcannibals;//Cannibals
-      //Validation
-      if (boatmissionaries + boatcannibals >= 3) {
-        cout << "ONLY 2 PEOPLE ALLOWED ON THE BOAT" << '\n' << endl;
-        confirmationQA = 'n';
-      } else if (boatmissionaries + boatcannibals <= 0) {
-        cout << "NO PEOPLE ON THE BOAT" << '\n' << endl;
-        confirmationQA = 'n';
-      } else {
+      cout << "How many missionaries get " << boatcondition  << " the boat?" << '\n';
+      cin >> promptmissionaries;//Misionaries
+      cout << "How many cannibals get " << boatcondition << " the boat?" << '\n';
+      cin >> promptcannibals;//Cannibals
+
+      //Getting on / off the boat
+      if (boatstatus % 4 == 0 || boatstatus % 4 == 2) {
         cout << "Ready to go? (Y/N)" << '\n';
         cin >> confirmationQA;
         cout << '\n';
+      } else {
+        cout << "Ready to get off? (Y/N)" << '\n';
+        cin >> confirmationQA;
+
       }
     } while(confirmationQA == 'n' || confirmationQA == 'N');
 
-
-
-    //Loop Stopper
-    leftcannibals = 3;
-    leftmissionaries = 3;
-
-  } while(leftcannibals != 3 && leftmissionaries != 3);
+    //Calculating people moved;
+    if(boatstatus % 4 == 0){
+      //Assign to boat
+      boatmissionaries += promptmissionaries;
+      boatcannibals += promptcannibals;
+      //Subtract from field
+      rightcannibals -= promptcannibals;
+      rightmissionaries -= promptmissionaries;
+    }
+    else if(boatstatus % 4 == 2){
+      //Assign to boat
+      boatmissionaries += promptmissionaries;
+      boatcannibals += promptcannibals;
+      //Subtract from field
+      leftcannibals -= promptcannibals;
+      leftmissionaries -= promptmissionaries;
+    }
+    else if(boatstatus % 4 == 1){
+      //Take from boat
+      boatmissionaries -= promptmissionaries;
+      boatcannibals -= promptcannibals;
+      //Add to field
+      leftcannibals += promptcannibals;
+      leftmissionaries += promptmissionaries;
+    }
+    else{
+      //Take from boat
+      boatmissionaries -= promptmissionaries;
+      boatcannibals -= promptcannibals;
+      //Add to field
+      rightcannibals += promptcannibals;
+      rightmissionaries += promptmissionaries;
+    }
+  }
   //Loop and game ends as all cannibals and missionaries already at the other side
 
   return 0;
